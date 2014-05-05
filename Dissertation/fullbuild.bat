e2f80f1148866a0d0e60e578fec66943c2b608d0
Caitlin Wilks <cat@wilks.so>
2014-05-05 13:14:32 +0100

    biblatex-apa usingness!!!

--- Dissertation/fullbuild.bat
+++ Dissertation/fullbuild.bat
@@ -11,6 +11,6 @@ mkdir bin
 
 :: Run latex, then bibtex, then latex twice again
 pdflatex %DOCUMENT_NAME%.tex -output-directory=./bin -aux-directory=./int
-bibtex int/%DOCUMENT_NAME% -include-directory=./int
+biber -U int/%DOCUMENT_NAME%
 pdflatex %DOCUMENT_NAME%.tex -output-directory=./bin -aux-directory=./int
 pdflatex %DOCUMENT_NAME%.tex -output-directory=./bin -aux-directory=./int
